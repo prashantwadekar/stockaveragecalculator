8c98ed5cdd2b21eb8557a59f30c5ef6aed0d5a14
PRASHANT ANANT WADEKAR <prashantanantwadekar@gmail.com>
2025-12-11 00:13:19 +0530

    Updated project

--- src/app/stock-average/stock-average.component.ts
+++ src/app/stock-average/stock-average.component.ts
@@ -3,17 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stock-average',
   templateUrl: './stock-average.component.html',
-  styleUrls: ['./stock-average.component.css']
+  styleUrls: ['./stock-average.component.css'],
 })
 export class StockAverageComponent {
-
   // First purchase
-  units1: number | null = 1;
-  price1: number | null = 1;
+  units1: number | null = 0;
+  price1: number | null = 0;
 
   // Second purchase
-  units2: number | null = 1;
-  price2: number | null = 1;
+  units2: number | null = 0;
+  price2: number | null = 0;
 
   // Results
   totalUnits: number = 0;
@@ -27,7 +26,8 @@ export class StockAverageComponent {
     this.totalUnits = (this.units1 ?? 0) + (this.units2 ?? 0);
     this.totalAmount = invest1 + invest2;
 
-    this.avgPrice = this.totalUnits > 0 ? this.totalAmount / this.totalUnits : 0;
+    this.avgPrice =
+      this.totalUnits > 0 ? this.totalAmount / this.totalUnits : 0;
   }
 
   clear() {
